bedaba047e26645482947791427645adad7bd26e
terencereilly <terence@olta.art>
2024-11-25 23:13:40 +0000

    share

--- config.js
+++ config.js
@@ -1,97 +1,136 @@
 {
-    "title": "vertices-berlin",
-    "contentUrl": "https://vertices-olta.vercel.app/",
-    "description": "test description",
-    "credits": [
-      {
-        "name": "test",
-        "role": "",
-        "address": ""
-      }
-    ],
-    "admin": "dhw4tVkCwdD39XuUlna530Vy5tT2Xz6NId5DHysFQCI",
-    "approvedMinters": {
-      "dhw4tVkCwdD39XuUlna530Vy5tT2Xz6NId5DHysFQCI": true
-    },
-    "balances": {
-      "dhw4tVkCwdD39XuUlna530Vy5tT2Xz6NId5DHysFQCI": {
-        "dataType": "BigInt",
-        "value": "0"
-      }
-    },
-    "fee": {
+  "title": "anim4",
+  "contentUrl": "https://vertices-partnership-01.vercel.app/",
+  "description": "test description",
+  "credits": [
+    {
+      "name": "test",
+      "role": "",
+      "address": ""
+    }
+  ],
+  "admin": "VTceeEl3yRiAKvBK2fKgV4g-klvzA6oNeWsbA2gjsa8",
+  "approvedMinters": {
+    "VTceeEl3yRiAKvBK2fKgV4g-klvzA6oNeWsbA2gjsa8": true
+  },
+  "balances": {
+    "VTceeEl3yRiAKvBK2fKgV4g-klvzA6oNeWsbA2gjsa8": {
       "dataType": "BigInt",
       "value": "0"
-    },
-    "isOpen": true,
-    "projectState": {
-      "configs": {
-        "vertices": {
-          "name": "vertices",
-          "type": "object",
-          "properties": {
-            "x": {
-              "name": "x",
-              "type": "bigint",
-              "min": "-10000000",
-              "max": "10000000",
-              "update": "set"
-            },
-            "y": {
-              "name": "y",
-              "type": "bigint",
-              "min": "-10000000",
-              "max": "10000000",
-              "update": "set"
-            },
-            "z": {
-              "name": "z",
-              "type": "bigint",
-              "min": "-10000000",
-              "max": "10000000",
-              "update": "set"
-            },
-            "lineColor": {
-              "name": "lineColor",
-              "type": "bigint",
-              "min": "0",
-              "max": "15658734",
-              "update": "set"
-            },
-            "vertexColor": {
-              "name": "vertexColor",
-              "type": "bigint",
-              "min": "0",
-              "max": "15658734",
-              "update": "set"
-            }
+    }
+  },
+  "fee": {
+    "dataType": "BigInt",
+    "value": "0"
+  },
+  "isOpen": true,
+  "projectState": {
+    "configs": {
+      "vertices": {
+        "name": "vertices",
+        "type": "object",
+        "properties": {
+          "x": {
+            "name": "x",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
           },
-          "permissions": {
-            "create": "open",
-            "createType": "set",
-            "update": "open",
-            "updateType": "set",
-            "remove": "admin",
-            "removeType": "set"
+          "y": {
+            "name": "y",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
+          },
+          "z": {
+            "name": "z",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
+          },
+          "lineColor": {
+            "name": "lineColor",
+            "type": "bigint",
+            "min": "0",
+            "max": "15658734",
+            "update": "set"
+          },
+          "vertexColor": {
+            "name": "vertexColor",
+            "type": "bigint",
+            "min": "0",
+            "max": "15658734",
+            "update": "set"
+          },
+          "cameraX": {
+            "name": "cameraX",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
+          },
+          "cameraY": {
+            "name": "cameraY",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
+          },
+          "cameraZ": {
+            "name": "cameraZ",
+            "type": "bigint",
+            "min": "-10000000",
+            "max": "10000000",
+            "update": "set"
           }
+        },
+        "permissions": {
+          "create": "open",
+          "createType": "set",
+          "update": "open",
+          "updateType": "set",
+          "remove": "admin",
+          "removeType": "set"
         }
-      },
+      }
+    },
+    "collections": {
+      "vertices": {
+        "0": {
+          "_id": 0,
+          "_creator": "0x",
+          "x": "9329371n",
+          "y": "-5675173n",
+          "z": "3775030n",
+          "lineColor": "8902992n",
+          "vertexColor": "3807313n",
+          "cameraX": "1231115n",
+          "cameraY": "1099339n",
+          "cameraZ": "-4738600n"
+        },
       "collections": {
         "vertices": {
-  
         }
-      }
-    },
-    "canEvolve": true,
-    "evolve": null,
-    "evaluationManifest": {
-      "evaluationOptions": {
-        "allowBigInt": true
+  
       }
     }
+  },
+  "canEvolve": true,
+  "evolve": null,
+  "evaluationManifest": {
+    "evaluationOptions": {
+      "allowBigInt": true
+    }
   }
+}
 
 
+
+
+// Details 
 
 Collection: vertices
 https://vertices-partnership-01.vercel.app/
